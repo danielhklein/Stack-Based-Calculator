02fd87bc41c146f9ca9a3869f38948f5f2403f29
danielhklein <danielklein@utexas.edu>
2015-07-11 17:34:50 -0500

    Finished calculator functionality and fixed constraints

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,16 +12,62 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMIddleOfTypingANumber: Bool = false
+    var userIsTyping: Bool = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        if userIsInTheMIddleOfTypingANumber {
+        if userIsTyping {
             display.text = display.text! + digit
         }
         else {
             display.text = digit
-            userIsInTheMIddleOfTypingANumber = true
+            userIsTyping = true
+        }
+    }
+    
+    var opStack = Array<Double>()
+    
+    @IBAction func returnAction() {
+        userIsTyping = false
+        opStack.append(displayVal)
+    }
+    
+    var displayVal: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+    }
+    
+    @IBAction func op(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsTyping {
+            returnAction()
+        }
+        switch operation {
+        case "+": performOp {$0 + $1}
+        case "-": performOp {$1 - $0}
+        case "×": performOp {$0 * $1}
+        case "÷": performOp {$1 / $0}
+        case "√": performOp {sqrt($0)}
+        default: break
+        }
+    }
+    
+    private func performOp(operation: (Double, Double) -> Double) {
+        if opStack.count >= 2 {
+            displayVal = operation(opStack.removeLast(), opStack.removeLast())
+            returnAction()
+        }
+    }
+    
+    private func performOp(operation: Double -> Double) {
+        if opStack.count >= 1 {
+            displayVal = operation(opStack.removeLast())
+            returnAction()
         }
     }
 }
